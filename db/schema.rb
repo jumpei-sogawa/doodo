f0532022edb3d5f9ff65452d5daa4382aa405201
jumpei-sogawa <jumpe123oon@gmail.com>
2020-03-30 17:59:54 +0900

    devise installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_162043) do
+ActiveRecord::Schema.define(version: 2020_03_30_085418) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -64,6 +64,22 @@ ActiveRecord::Schema.define(version: 2020_03_29_162043) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.string "display_name"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
