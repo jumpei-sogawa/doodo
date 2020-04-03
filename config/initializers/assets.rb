98f828f0f720c5065b2c108947f98e0a4c642ed6
jumpei-sogawa <jumpe123oon@gmail.com>
2020-04-04 01:19:44 +0900

    template installed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,16 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( browser.min.js )
+Rails.application.config.assets.precompile += %w( breakpoints.min.js )
+Rails.application.config.assets.precompile += %w( util.js )
+Rails.application.config.assets.precompile += %w( main.js )
