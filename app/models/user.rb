eb9c54bb58d06ca0126b07c50802404eb4ca46c3
jumpei-sogawa <jumpe123oon@gmail.com>
2020-04-12 17:52:51 +0900

    registar with username validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username,
+    uniqueness: { case_sensitive: :false },
+    length: { minimum: 4, maximum: 20 },
+    format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
 end
