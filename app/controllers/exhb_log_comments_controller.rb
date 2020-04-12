67deb33070ba3a21da2f1a1c6391c05362bd85b1
jumpei-sogawa <jumpe123oon@gmail.com>
2020-04-13 00:33:33 +0900

    all pages PC UI modified

--- app/controllers/exhb_log_comments_controller.rb
+++ app/controllers/exhb_log_comments_controller.rb
@@ -1,5 +1,5 @@
 class ExhbLogCommentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create]
 
   def create
     exhb_log_comment = ExhbLogComment.new(exhb_log_comment_params)
