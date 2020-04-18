5be956b4f0351e0279ced56f237263d888d92282
jumpei-sogawa <jumpe123oon@gmail.com>
2020-04-18 18:55:56 +0900

    set after sign out path

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,4 +25,13 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+    def after_sign_in_path_for(resource)
+      mypage_path
+    end
+
+    def after_sign_out_path_for(resource)
+      new_user_session_path
+    end
 end
