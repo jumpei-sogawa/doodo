378d7f45d078a36c70fa66a7fae0275a16fdb27b
jumpei-sogawa <jumpe123oon@gmail.com>
2020-04-30 01:19:34 +0900

    home tab ui modified

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,7 @@ class PagesController < ApplicationController
   def home
     @title = "アート・絵画・美術館・展覧会の口コミ検索サイト | stART"
     @description = "アート・展覧会の口コミサイト「stART」のトップページです。stARTはアート・絵画・美術館・展覧会・美術展の口コミが見れる検索サイトです。上野美術館、国立新美術館、国立西洋美術館、六本木など、各地で開催されている展覧会の最新情報をご案内。"
-    @exhibitions = Exhibition.is_open.order("star DESC NULLS LAST").first(4)
-    @arts = Art.order("star DESC NULLS LAST").first(4)
+    @exhb_logs = ExhbLog.first(24)
   end
 
   def trend
