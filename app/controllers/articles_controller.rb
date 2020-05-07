28fe469ab54efc2e5b7b831a4b6c4d7496acc9f8
jumpei-sogawa <jumpe123oon@gmail.com>
2020-05-08 00:59:51 +0900

    article funstions UI modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,10 +52,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1.json
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to articles_admin_path
   end
 
   def publish
