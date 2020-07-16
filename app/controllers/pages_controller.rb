37eb7496d38af8856a961cce1e961fd056ee0de6
jumpei-sogawa <jumpe123oon@gmail.com>
2020-07-17 00:01:57 +0900

    検索ページにももっと見る機能を実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @description = "アート・展覧会の感想まとめサイト「doodo」のトップページです。doodoはアート・絵画・美術館・展覧会・美術展の感想が見れる検索サイトです。六本木、国立新美術館、上野、国立西洋美術館など、各地で開催されている展覧会の最新情報をご案内。"
     @exhb_logs = ExhbLog.order(id: "DESC").page(params[:page]).per(20)
     @exhibitions = Exhibition.is_open.order("star DESC NULLS LAST").page(params[:page]).per(20)
-    @arts = Art.order("star DESC NULLS LAST").page(params[:page]).per(10)
+    @arts = Art.order("star DESC NULLS LAST").page(params[:page]).per(20)
 
     case params[:type]
     when 'exhb_logs', 'exhibitions', 'arts'
@@ -22,23 +22,30 @@ class PagesController < ApplicationController
     @description = "アート・展覧会の感想まとめサイト「doodo」の検索ページです。doodoはアート・絵画・美術館・展覧会・美術展の感想が見れる検索サイトです。六本木、国立新美術館、上野、国立西洋美術館など、各地で開催されている展覧会の最新情報をご案内。"
 
     if params[:museum_area].present? || params[:museum_name].present?
-      museums = Museum.where("address LIKE ?", "%#{params[:museum_area]}%").where("name LIKE ?", "%#{params[:museum_name]}%")
-      @museums = museums.sort do |a,b|
-        if !a.exhibitions.last.star.present?
-          1
-        elsif !b.exhibitions.last.star.present?
-          -1
-        else
-          b <=> a
-        end
-      end
+      @museums = Museum
+                     .where.not(id: 1)
+                     .where("address LIKE ?", "%#{params[:museum_area]}%")
+                     .where("name LIKE ?", "%#{params[:museum_name]}%")
+                     .page(params[:page]).per(24)
+      # @museums = @museums.sort do |a,b|
+      #   if !a.exhibitions.last.star.present?
+      #     1
+      #   elsif !b.exhibitions.last.star.present?
+      #     -1
+      #   else
+      #     b <=> a
+      #   end
+      # end
     else
-      @museums = Museum.includes(:exhibitions).order("exhibitions.star DESC NULLS LAST")
+      @museums = Museum
+                     .where.not(id: 1)
+                     .includes(:exhibitions)
+                     .order("exhibitions.star DESC NULLS LAST")
+                     .page(params[:page]).per(20)
     end
-    @museums = @museums.select { |museum| museum.id != 1 }.first(20)
 
     if params[:any].present?
-      @exhibitions = Exhibition.search_by_any(params).sort do |a,b|
+      @exhibitions = Exhibition.where.not(id: 1).search_by_any(params).compact.sort do |a,b|
         if !a.star.present?
           1
         elsif !b.star.present?
@@ -48,7 +55,7 @@ class PagesController < ApplicationController
         end
       end
     elsif params[:exhb_area].present? || params[:exhb_name].present? || params[:exhb_date].present?
-      @exhibitions = Exhibition.search_by(params).sort do |a,b|
+      @exhibitions = Exhibition.where.not(id: 1).search_by(params).compact.sort do |a,b|
         if a.blank?
           1
         elsif b.blank?
@@ -64,18 +71,11 @@ class PagesController < ApplicationController
         end
       end
     else
-      @exhibitions = Exhibition.order("star DESC NULLS LAST")
+      @exhibitions = Exhibition.where.not(id: 1).order("star DESC NULLS LAST").page(params[:page]).per(20)
     end
-    @exhibitions = @exhibitions.select do |exhb|
-      if exhb.present?
-        exhb.id != 1
-      end
-    end
-    @exhibitions = @exhibitions.first(20)
 
     if params[:art_name].present? || params[:artist_name].present?
-      arts = Art.search_by(params).first(20)
-      @arts = arts.sort do |a,b|
+      @arts = Art.search_by(params).compact.sort do |a,b|
         if !a.star.present?
           1
         elsif !b.star.present?
@@ -85,7 +85,12 @@ class PagesController < ApplicationController
         end
       end
     else
-      @arts = Art.order("star DESC NULLS LAST").first(20)
+      @arts = Art.order("star DESC NULLS LAST").page(params[:page]).per(20)
+    end
+
+    case params[:type]
+    when 'search_museums', 'search_exhibitions', 'search_arts'
+      render "#{params[:type]}"
     end
   end
 
